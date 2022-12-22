4e27bb097525ed297ed15cf365d6bb00e1ff85a1
VerditeLabs <19700662+VerditeLabs@users.noreply.github.com>
2022-12-22 11:33:42 -0800

    do some data processing

--- main.py
+++ main.py
@@ -29,8 +29,33 @@ def main():
             id_name_map[id] = name
             name_lit_map[name].add(lit)
         print('\n'.join(sorted(names)))
+        print("preprocessed names")
+
+def scrape():
+    import pandas as pd
+    from bs4 import BeautifulSoup
+    import requests
+    import time
+    import random
+    for ingr in sanitized_ingredients:
+        time.sleep(random.randrange(1, 10) / 10 + 1)
+        #print("trying",ingr)
+        f = requests.get("https://incidecoder.com/ingredients/" + ingr)
+        soup = BeautifulSoup(f.text)
+        import re
+        txt = soup.get_text()
+        txt = re.sub(r"\s+", ' ', txt)
+        if 'this page does not seem' in txt:
+            #print(ingr," not found")
+            continue
+        else:
+            print(ingr)
+            #print(soup.get_text())
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+    #scrape()
